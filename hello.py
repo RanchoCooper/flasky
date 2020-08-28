1b017c1dc8de97660e38f71b5318a73bdd7e1db6
Rancho <ranchocooper@gmail.com>
2020-08-29 02:55:50 +0800

    use template

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from flask import Flask, request
+from flask import Flask, render_template, request
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -10,12 +10,12 @@ manager = Manager(app)
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-agent')
-    return '<h1>hello, world!</h1>\nyour browser is %s' % user_agent
+    return render_template('index.html', user_agent=user_agent)
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
