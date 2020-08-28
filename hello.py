0bbf83427adeb46b50a4c523d2ee468eb7384074
Rancho <ranchocooper@gmail.com>
2020-08-29 05:36:54 +0800

    up

--- hello.py
+++ hello.py
@@ -3,25 +3,24 @@
 from datetime import datetime
 
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_moment import Moment
 from flask_script import Manager
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
-def index():
-    user_agent = request.headers.get('User-agent')
-    return render_template('index.html', user_agent=user_agent, current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+class NameForm(Form):
+    name = StringField('what is your name?', validators=[DataRequired()])
+    submit = SubmitField('submit')
 
 
 @app.errorhandler(404)
@@ -32,6 +31,20 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
