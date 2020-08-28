f7a6c41d92e78f9ec49366e887780e3c62977deb
Rancho <ranchocooper@gmail.com>
2020-08-29 03:52:24 +0800

    use moment

--- hello.py
+++ hello.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from flask import Flask, render_template, request
+from datetime import datetime
+
 from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request
+from flask_moment import Moment
 from flask_script import Manager
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-agent')
-    return render_template('index.html', user_agent=user_agent)
+    return render_template('index.html', user_agent=user_agent, current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
