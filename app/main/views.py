de8a1ed2296982e247b4d24864bf1eab57dd63c4
Rancho <ranchocooper@gmail.com>
2020-08-30 00:26:52 +0800

    large file structure

---
+++ app/main/views.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from flask import current_app, redirect, render_template, session, url_for
+
+from app import db
+from app.email import send_email
+from app.main import main
+from app.models import User
+from hello import NameForm
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
---
+++ manage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import os
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell
+
+from app import create_app, db
+from app.models import Role, User
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
+
+
+@manager.command
+def test():
+    """run the unit tests"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
