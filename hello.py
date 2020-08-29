72a25e393694d8443c94de5ffb1cbfdb8510e57d
Rancho <ranchocooper@gmail.com>
2020-08-29 15:24:42 +0800

    use sqlalchemy

--- hello.py
+++ hello.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import os
 from datetime import datetime
 
 from flask_bootstrap import Bootstrap
 from flask import flash, Flask, redirect, render_template, request, session, url_for
 from flask_moment import Moment
 from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql://root@localhost/flasky'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -21,6 +29,26 @@ moment = Moment(app)
 class NameForm(Form):
     name = StringField('what is your name?', validators=[DataRequired()])
     submit = SubmitField('submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %d: %s>' % (self.id, self.name)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %d: %s>' % (self.id, self.username)
 
 
 @app.errorhandler(404)
